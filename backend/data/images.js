9ccc3643930e85b0f5c5ebfadfa9f1b763ab5f35
Pilar Muñoz <86972128+pibmunoz@users.noreply.github.com>
2023-05-25 19:49:30 -0400

    api images interface

---
+++ backend/data/images.js
@@ -0,0 +1,22 @@
+const images = [
+    {
+        _id: 1,
+        name: 'desktop',
+        path: 'desktop-banner.jpg',
+        alt: 'Mujer apicultora',
+    },
+    {
+        _id: 2,
+        name: 'mobile',
+        path: 'mobile-banner.jpg',
+        alt: 'Abejas en panal',
+    },
+    {
+        _id: 3,
+        name: 'footer',
+        path: 'footer.jpg',
+        alt: 'Abeja polinizando flor',
+    }
+]
+
+module.exports = images;
